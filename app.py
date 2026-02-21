0ac61a1b4be74c507a2ce60c35b1d3ff60e88b8c
coralogy <60720799+coralogy@users.noreply.github.com>
2026-02-21 22:25:13 +0800

    add some pictures

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app.")
-st.write("another graph")
+st.title("Hello!")
+st.write("This is acro schedule.")
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+st.write("does this work")
